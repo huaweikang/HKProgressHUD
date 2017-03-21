e1aa26b12fa67a7653be6302610151aa5f8306e3
Huawei Kang <hackerkang1990@gmail.com>
2017-03-21 11:39:22 +0800

    add HubDemo project.

---
+++ HubDemo/HubDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HubDemo
+//
+//  Created by kang huawei on 2017/3/21.
+//  Copyright © 2017年 huaweikang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
