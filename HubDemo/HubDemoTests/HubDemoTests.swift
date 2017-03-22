89587f0e9cd8e6279a5425e33e34db4f202eccaf
Huawei Kang <hackerkang1990@gmail.com>
2017-03-22 22:49:27 +0800

    add test non animation.

--- HubDemo/HubDemoTests/HubDemoTests.swift
+++ HubDemo/HubDemoTests/HubDemoTests.swift
@@ -21,16 +21,31 @@ class HubDemoTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testNonAnimatedConvenienceHubPresentation() {
+        let rootViewController = UIApplication.shared.keyWindow?.rootViewController
+        let rootView = rootViewController?.view
+        
+        let hub = ProgressHub.show(addedToView: rootView!, animated: false)
+        XCTAssertNotNil(hub, "Hub should be created.")
+        checkHubVisible(hub, rootView: rootView!)
+        XCTAssertFalse((hub.bezelView?.layer.animationKeys()?.contains("opacity"))!, "The opcity should not be animated.")
+        XCTAssertTrue(ProgressHub.hide(addedToView: rootView!, animated: false), "Hub should be found and removed")
+        checkHubHiddenAndRemoved(hub, rootView: rootView!)
+        XCTAssertFalse(ProgressHub.hide(addedToView: rootView!, animated: false), "A subsequent Hub hide operation should fail")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func checkHubVisible(_ hub: ProgressHub, rootView: UIView) {
+        XCTAssertTrue(hub.superview === rootView, "hub not be added to the view")
+        XCTAssertTrue(ProgressHub.hubForView(rootView) === hub, "Hub should be found via the operation")
+        XCTAssertTrue(hub.alpha == 1.0, "hub should be visible.")
+        XCTAssertFalse(hub.isHidden, "hub should be visible.")
+        XCTAssertTrue(hub.bezelView?.alpha == 1.0, "hub bezel view should be visible")
+    }
+    
+    func checkHubHiddenAndRemoved(_ hub: ProgressHub, rootView: UIView) {
+        XCTAssertFalse(rootView.subviews.contains(hub), "The Hub should be removed.")
+        XCTAssertTrue(hub.alpha == 0.0, "Hub should be invisible")
+        XCTAssertNil(hub.superview, "Hub should not have a superview")
     }
     
 }
